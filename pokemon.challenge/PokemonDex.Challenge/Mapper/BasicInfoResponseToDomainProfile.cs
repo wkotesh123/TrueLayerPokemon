d68654ad2c9706cdfaa14dfdf341ab804e53cff8
Kotteeswaran Mahalingam <Kotteeswaran.Mahalingam@cognizant.com>
2022-05-10 19:41:57 +0100

    launch setting

--- pokemon.challenge/PokemonDex.Challenge/Mapper/BasicInfoResponseToDomainProfile.cs
+++ pokemon.challenge/PokemonDex.Challenge/Mapper/BasicInfoResponseToDomainProfile.cs
@@ -14,7 +14,7 @@ namespace PokemonDex.Challenge.Mapper
             CreateMap<PokemonApiBasicInfoResponse, PokemonDexBasicInfoResponse>()
                .ForMember(dest => dest.Name, src => src.MapFrom(s => s.Name))
                .ForMember(dest => dest.IsLegendary, src => src.MapFrom(s => s.IsLegendary))
-               .ForMember(dest => dest.StandardDescription, src => src.MapFrom(s => s.FlavourTextEntries.Where(a=>a.Language.Name=="en").Take(1).Select(a => a.FlavorText).First()))
+               .ForMember(dest => dest.StandardDescription, src => src.MapFrom(s => s.FlavourTextEntries.Where(a=>a.Language.Name=="en").Take(1).Select(a => a.FlavorText).FirstOrDefault().Replace("\r", " ").Replace("\n", " ")))
                .ForMember(dest => dest.Habitat, src => src.MapFrom(s => s.HabitatDetails.Name));
 
         }
